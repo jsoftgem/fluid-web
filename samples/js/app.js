f6c94a73ae968204be1bb09df4c647683a28830c
jerico <jerico@jsofttechnologies.com>
2015-04-18 05:53:52 +0800

    updates

--- samples/js/app.js
+++ samples/js/app.js
@@ -3,5 +3,9 @@
  */
 angular.module("mainApp", ["fluid"])
     .controller("mainCtrl", ["$scope", "fluidHttpService", "fluidFrameService", function (s, fhs, ffs) {
-        ffs.addTask("app/Module1/module1.json");
+        s.openTask = function (url) {
+            ffs.addTask(url);
+        }
+
+
     }]);
