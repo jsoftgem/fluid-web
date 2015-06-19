4a56bc86ec44eaf31d868d12e25d668b737c8821
Niko de Guzman <nikodeguzman@gmail.com>
2015-06-19 18:32:34 +0800

    updates

--- src/js/modules/fluid-progress.js
+++ src/js/modules/fluid-progress.js
@@ -2,7 +2,7 @@
  * Created by Jerico on 6/19/2015.
  */
 angular.module("fluidProgress", [])
-    .directive("fluidProgress", ["$templateCache", function (tc) {
+    .directive("fluidProgress", ["$templateCache", "fluidProgressService", "FluidProgress", function (tc, fps, FluidProgress) {
         return {
             require: "^fluidFrame",
             restrict: "E",
@@ -11,27 +11,64 @@ angular.module("fluidProgress", [])
             replace: true,
             transclude: true,
             link: function (scope, elem, attr) {
+
+                scope.runners = [];
+
                 if (attr.id) {
                     var id = elem.attr("id");
-                    elem.attr(id + "_progress");
+                    elem.attr("id", id + "_progress");
+                    var elemId = elem.attr("id");
+                    scope.progres = FluidProgress({id: elemId});
+                } else {
+                    throw "Id attribute is required.";
                 }
 
-                if (attr.name) {
-                    var name = elem.attr("name");
-                    elem.attr(name + "_progress");
-                }
+
+                element.on(element.attr("id"), function () {
+                    var progess = element.scope().progress;
+
+                });
+
+
             }
         }
     }])
-    .factory("FluidProgress", [function () {
+    .factory("FluidProgress", ["fluidProgressService", function (fps) {
 
         var fluidProgress = function (param) {
 
             if (param.id) {
-                this.id = param.id;
-                this.$ = function () {
-                    return $("#" + this.param.id+"_progress");
+
+                if (fps.getFluidProgress(param.id) != null) {
+                    return fps.getFluidProgress(param.id);
+                } else {
+                    this.id = param.id;
+                    this.run = function (name, loadFn, sleep) {
+                        var runner = {};
+                        runner.name = name;
+                        runner.load = loadFn;
+                        runner.message = "Loading please wait...";
+                        runner.cancel = function () {
+                            this.cancelled = true;
+                        }
+                        runner.ok = function () {
+                            this.done = true;
+                        }
+                        runner.sleep = 0;
+                        if (this.runners === undefined) {
+                            this.runners = [];
+                        }
+                        this.runners.push(runner);
+                        var element = angular.element(this.$());
+                        element.triggerHandler(element.attr("id"));
+                    }
+                    this.$ = function () {
+                        return $("#" + this.param.id + "_progress");
+                    }
+                    fps.addFluidProgress(this);
                 }
+            } else {
+                throw "param id is required";
             }
 
 
@@ -39,4 +76,18 @@ angular.module("fluidProgress", [])
         }
 
         return fluidProgress;
+    }])
+    .service("fluidProgressService", [function () {
+        this.progressObjects = [];
+
+        this.addFluidProgress = function (progress) {
+            this.progressObjects[progres.id + "_progress"] = progress;
+        }
+        this.getFluidProgress = function (id) {
+            this.progressObjects[id + "_progress"];
+        }
+
+        this.clearProgress = function (id) {
+            this.progressObjects[id] = undefined;
+        }
     }]);
